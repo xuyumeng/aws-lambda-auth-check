3ba03ea34733090ce1b92382bd2734b854b4fc57
Yumeng Xu <yumengxu1994@icloud.com>
2019-07-23 19:36:41 +0800

    fix bugs

--- index.js
+++ index.js
@@ -2,74 +2,73 @@ const AWS = require("aws-sdk");
 const co = require("await-to-js").default;
 
 class Auth {
-	UserPoolId = null;
-	cognitoClient = null;
-	user = null;
-	groups = null;
+    constructor(Region, UserPoolId) {
+        this.cognitoClient = new AWS.CognitoIdentityServiceProvider({
+            region: process.env.REGION
+        });
+        this.UserPoolId = UserPoolId;
+        this.user = null;
+        this.groups = null;
+    }
 
-	constructor(Region, UserPoolId) {
-		this.cognitoClient = new AWS.CognitoIdentityServiceProvider({
-			region: process.env.REGION
-		});
-		this.UserPoolId = UserPoolId
-	}
+    get Username() {
+        return this.user.Username;
+    }
 
-	get Username() {
-		return this.user.Username;
-	}
+    async getUserInfo(sub) {
+        let err, data;
 
-	async getUserInfo(sub) {
-		let err
+        const request = {
+            UserPoolId: this.UserPoolId,
+            Filter: `sub = "${sub}"`,
+            Limit: 1
+        };
+        [err, data] = await co(this.cognitoClient.listUsers(request).promise());
+        if (err) {
+            return [err];
+        }
 
-		const request = {
-			UserPoolId,
-			Filter: `sub = "${sub}"`,
-			Limit: 1
-		};
-		[err, data] = await co(cognitoClient.listUsers(request).promise());
-		if (err) {
-			return [err];
-		}
+        this.user = data.Users[0];
+        return [err,this.user];
+    }
 
-		this.user = data.Users[0]
-		return [err,this.user];
-	}
+    async isAdmin(sub) {
+        let err;
+        if (!this.user) {
+            [err] = await this.getUserInfo(sub);
+        }
 
-	async isAdmin(sub) {
-		let err
-		if (!user) {
-			[err] = await getUserInfo(sub)
-		}
+        [err] = await this.getGroup();
 
-		[err] = await getGroup()
+        if (err) {
+            return [err];
+        }
 
-		if (err) {
-			return [err]
-		}
+        if (!this.groups.filter(function (e) { return e.GroupName === 'admin'})) {
+            return [err, false, this.user, this.groups];
+        } else {
+            return [err, true, this.user, this.groups];
+        }
+    }
 
-		if (!this.groups.filter(e => e.GroupName === 'admin')) {
-			return [err, false]
-		} else {
-			return [err, true]
-		}
-	}
+    // common functions
+    async getGroup() {
+        let err, data;
 
-	// common functions
-	async getGroup() {
-		let err;
+        const Username = this.user.Username;
 
-		const Username = this.user.Username;
+        [err, data] = await co(this.cognitoClient.adminListGroupsForUser({
+            UserPoolId: this.UserPoolId,
+            Username
+        }).promise());
+        if (err) {
+            console.error(err);
+            return [err];
+        }
 
-		[err, data] = await co(cognitoClient.adminListGroupsForUser({
-			this.UserPoolId,
-			Username
-		}).promise());
-		if (err) {
-			console.error(err);
-			return [err];
-		}
-
-		this.groups = data.Groups;
-		return [err, this.groups];
-	}
+        this.groups = data.Groups;
+        return [err, this.groups];
+    }
 }
+
+module.exports = Auth;
